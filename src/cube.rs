2ca69b934704298e28b722f902bc564f49161e7b
Tyler Southwick <tyler@northfuse.io>
2019-12-17 23:17:27 -0800

    fix rotations and turns

--- src/cube.rs
+++ src/cube.rs
@@ -5,6 +5,9 @@ pub enum Color {
     RED, YELLOW, WHITE, GREEN, BLUE, ORANGE,
 }
 
+use Color::*;
+
+
 impl fmt::Display for Color {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", match self {
@@ -48,6 +51,61 @@ impl Side {
                 [color, color, color]]
         }
     }
+
+    fn rotate_left(&mut self) {
+        self.rotate_right();
+        self.rotate_right();
+        self.rotate_right();
+    }
+
+    fn rotate_right(&mut self) {
+        /*
+           0 1 2    6 3 0
+           3 4 5 -> 7 4 1
+           6 7 8    8 5 2
+        */
+        let square0 = self.squares[0][0];
+        let square1 = self.squares[0][1];
+        let square2 = self.squares[0][2];
+        let square3 = self.squares[1][0];
+        let square5 = self.squares[1][2];
+        let square6 = self.squares[2][0];
+        let square7 = self.squares[2][1];
+        let square8 = self.squares[2][2];
+
+/*
+        println!("square0: {}", square0);
+        println!("square1: {}", square1);
+        println!("square2: {}", square2);
+        println!("square3: {}", square3);
+        println!("square4: {}", self.squares[1][1]);
+        println!("square5: {}", square5);
+        println!("square6: {}", square6);
+        println!("square7: {}", square7);
+        println!("square8: {}", square8);
+        */
+
+        self.squares[0][0] = square6;
+        self.squares[0][1] = square3;
+        self.squares[0][2] = square0;
+        self.squares[1][0] = square7;
+        self.squares[1][2] = square1;
+        self.squares[2][0] = square8;
+        self.squares[2][1] = square5;
+        self.squares[2][2] = square2;
+
+/*
+        println!("square00: {}", self.squares[0][0]);
+        println!("square01: {}", self.squares[0][1]);
+        println!("square02: {}", self.squares[0][2]);
+        println!("square10: {}", self.squares[1][0]);
+        println!("square11: {}", self.squares[1][1]);
+        println!("square12: {}", self.squares[1][2]);
+        println!("square20: {}", self.squares[2][0]);
+        println!("square21: {}", self.squares[2][1]);
+        println!("square22: {}", self.squares[2][2]);
+        */
+    }
 }
 
 impl Cube {
@@ -66,16 +124,7 @@ impl Cube {
     }
 
     pub fn default() -> Self {
-        Cube {
-            sides: [
-                Side::new(Color::RED),
-                Side::new(Color::YELLOW),
-                Side::new(Color::WHITE),
-                Side::new(Color::GREEN),
-                Side::new(Color::BLUE),
-                Side::new(Color::ORANGE)
-            ]
-        }
+        Cube::new([BLUE, RED, YELLOW, ORANGE, WHITE, GREEN])
     }
 
     pub fn orientation_rotate_right(&mut self) {
@@ -115,6 +164,48 @@ impl Cube {
     pub fn orientation_rotate_down(&mut self) {
         self.orientation_rotate_up();
         self.orientation_rotate_up();
+    }
+
+    pub fn rotate_right(&mut self) {
+        //   0           1
+        //  1234   ---> 5204
+        //   5           3
+        //    |BBB|                 |RRR|
+        //    |BBB|                 |RRR|
+        //    |BBB|                 |RRR|
+        //|RRR|YYY|OOO|WWW|     |GGG|YYY|BBB|WWW|
+        //|RRR|YYY|OOO|WWW| --> |GGG|YYY|BBB|WWW|
+        //|RRR|YYY|OOO|WWW|     |GGG|YYY|BBB|WWW|
+        //    |GGG|                 |OOO|
+        //    |GGG|                 |OOO|
+        //    |GGG|                 |OOO|
+        //sides 2 & 4 do not change
+        //1 -> 0
+        //5 -> 1
+        //0 -> 3
+        //3 -> 5
+        let side0 = self.sides[0];
+        let side1 = self.sides[1];
+        let side3 = self.sides[3];
+        let side5 = self.sides[5];
+
+        self.sides[0] = side1;
+        self.sides[1] = side5;
+        self.sides[3] = side0;
+        self.sides[5] = side3;
+
+/*
+        println!("----------- before rotating");
+        self.print();
+        println!("----------- after rotating");
+        */
+        //the center has to rotate
+        self.sides[2].rotate_right();
+        self.sides[1].rotate_right();
+        self.sides[3].rotate_right();
+        self.sides[4].rotate_left();
+        self.sides[0].rotate_right();
+        self.sides[5].rotate_right();
     }
 
     pub fn move_f(&mut self) {
@@ -158,12 +249,65 @@ impl Cube {
         self.sides[5].squares[0][0] = right3;
         self.sides[5].squares[0][1] = right2;
         self.sides[5].squares[0][2] = right1;
+
+        self.sides[2].rotate_right();
     }
 
     pub fn move_fi(&mut self) {
         self.move_f();
         self.move_f();
         self.move_f();
+    }
+
+    pub fn move_r(&mut self) {
+        //    |RRR|                 |RRW|
+        //    |RRR|                 |RRW|
+        //    |RRR|                 |RRW|
+        //|YYY|WWW|GGG|BBB|     |YYY|WWO|GGG|RBB|
+        //|YYY|WWW|GGG|BBB| --> |YYY|WWO|GGG|RBB|
+        //|YYY|WWW|GGG|BBB|     |YYY|WWO|GGG|RBB|
+        //    |OOO|                 |OOB|
+        //    |OOO|                 |OOB|
+        //    |OOO|                 |OOB|
+        let up1 = self.sides[0].squares[0][2];
+        let up2 = self.sides[0].squares[1][2];
+        let up3 = self.sides[0].squares[2][2];
+
+        let right1 = self.sides[4].squares[0][0];
+        let right2 = self.sides[4].squares[1][0];
+        let right3 = self.sides[4].squares[2][0];
+
+        let bottom1 = self.sides[5].squares[0][2];
+        let bottom2 = self.sides[5].squares[1][2];
+        let bottom3 = self.sides[5].squares[2][2];
+
+        let center02 = self.sides[2].squares[0][2];
+        let center12 = self.sides[2].squares[1][2];
+        let center22 = self.sides[2].squares[2][2];
+
+        self.sides[2].squares[0][2] = bottom1;
+        self.sides[2].squares[1][2] = bottom2;
+        self.sides[2].squares[2][2] = bottom3;
+
+        self.sides[0].squares[0][2] = center02;
+        self.sides[0].squares[1][2] = center12;
+        self.sides[0].squares[2][2] = center22;
+
+        self.sides[4].squares[0][0] = up3;
+        self.sides[4].squares[1][0] = up2;
+        self.sides[4].squares[2][0] = up1;
+
+        self.sides[5].squares[0][2] = right3;
+        self.sides[5].squares[1][2] = right2;
+        self.sides[5].squares[2][2] = right1;
+
+        self.sides[3].rotate_right();
+    }
+
+    pub fn move_ri(&mut self) {
+        self.move_r();
+        self.move_r();
+        self.move_r();
     }
 
     pub fn print(&self) {
@@ -199,10 +343,10 @@ impl fmt::Display for Cube {
             self.sides[3].squares[1][0], self.sides[3].squares[1][1], self.sides[3].squares[1][2],
             self.sides[4].squares[1][0], self.sides[4].squares[1][1], self.sides[4].squares[1][2],
             //third long row
-            self.sides[1].squares[2][0], self.sides[1].squares[1][1], self.sides[1].squares[2][2],
-            self.sides[2].squares[2][0], self.sides[2].squares[1][1], self.sides[2].squares[2][2],
-            self.sides[3].squares[2][0], self.sides[3].squares[1][1], self.sides[3].squares[2][2],
-            self.sides[4].squares[2][0], self.sides[4].squares[1][1], self.sides[4].squares[2][2],
+            self.sides[1].squares[2][0], self.sides[1].squares[2][1], self.sides[1].squares[2][2],
+            self.sides[2].squares[2][0], self.sides[2].squares[2][1], self.sides[2].squares[2][2],
+            self.sides[3].squares[2][0], self.sides[3].squares[2][1], self.sides[3].squares[2][2],
+            self.sides[4].squares[2][0], self.sides[4].squares[2][1], self.sides[4].squares[2][2],
             //last row
             prefix, self.sides[5].squares[0][0], self.sides[5].squares[0][1], self.sides[5].squares[0][2],
             prefix, self.sides[5].squares[1][0], self.sides[5].squares[1][1], self.sides[5].squares[1][2],
@@ -240,15 +384,25 @@ mod tests {
     }
 
     #[test]
-    fn rotate_right() {
-        let mut cube = Cube::new([RED, YELLOW, WHITE, GREEN, BLUE, ORANGE]);
+    fn orientation_rotate_right() {
+        let mut cube = Cube::new([RED, GREEN, YELLOW, BLUE, WHITE, ORANGE]);
         cube.orientation_rotate_right();
         //   1           1
         //  2345   ---> 3452
         //   6           6
         assert_eq!(
             format!("{}", cube),
-            format!("{}", Cube::new([RED, WHITE, GREEN, BLUE, YELLOW, ORANGE])),
+            format!("{}", Cube::new([RED, YELLOW, BLUE, WHITE, GREEN, ORANGE])),
+        );
+    }
+
+    #[test]
+    fn rotate_right() {
+        let mut cube = Cube::new([BLUE, YELLOW, ORANGE, WHITE, RED, GREEN]);
+        cube.rotate_right();
+        assert_eq!(
+            format!("{}", cube),
+            format!("{}", Cube::new([YELLOW, GREEN, ORANGE, BLUE, RED, WHITE])),
         );
     }
 
@@ -297,4 +451,129 @@ println!("{}", expected);
         );
     }
 
+    #[test]
+    fn move_r() {
+        //    |WWW|                 |WWB|
+        //    |WWW|                 |WWB|
+        //    |WWW|                 |WWB|
+        //|RRR|BBB|OOO|GGG|     |RRR|BBY|OOO|WGG|
+        //|RRR|BBB|OOO|GGG| --> |RRR|BBY|OOO|WGG|
+        //|RRR|BBB|OOO|GGG|     |RRR|BBY|OOO|WGG|
+        //    |YYY|                 |YYG|
+        //    |YYY|                 |YYG|
+        //    |YYY|                 |YYG|
+        let mut cube = Cube::new([WHITE, RED, BLUE, ORANGE, GREEN, YELLOW]);
+        cube.print();
+        cube.move_r();
+        println!("--------");
+        cube.print();
+        println!("--------");
+        let prefix = "    ";
+        let expected= format!("{}|WWB|\n\
+            {}|WWB|\n\
+            {}|WWB|\n\
+        |RRR|BBY|OOO|WGG|\n\
+        |RRR|BBY|OOO|WGG|\n\
+        |RRR|BBY|OOO|WGG|\n\
+        {}|YYG|\n\
+        {}|YYG|\n\
+        {}|YYG|\n", prefix, prefix, prefix, prefix, prefix, prefix);
+println!("{}", expected);
+        assert_eq!(format!("{}", cube), expected);
+    }
+
+    #[test]
+    fn move_ri_rotate_right() {
+    /*
+        |BBB|
+        |BBB|
+        |BBB|
+    |RRR|YYY|OOO|WWW|
+    |RRR|YYY|OOO|WWW|
+    |RRR|YYY|OOO|WWW|
+        |GGG|
+        |GGG|
+        |GGG|
+    ------ step 0 move ri --------
+        |BBW|
+        |BBW|
+        |BBW|
+    |RRR|YYB|OOO|GWW|
+    |RRR|YYB|OOO|GWW|
+    |RRR|YYB|OOO|GWW|
+        |GGY|
+        |GGY|
+        |GGY|
+    ------ step 0 rotate right --------
+        |RRR|
+        |RRR|
+        |RRR|
+    |GGG|YYY|BBB|WWW|
+    |GGG|YYY|BBB|WWW|
+    |YYY|BBB|WWW|GGG|
+        |OOO|
+        |OOO|
+        |OOO|
+    */
+        let cube = Cube::new([BLUE, RED, YELLOW, ORANGE, WHITE, GREEN]);
+        let mut scrambled_cube = cube.clone();
+        scrambled_cube.move_ri();
+        scrambled_cube.rotate_right();
+        println!("scrambled!");
+        scrambled_cube.print();
+        let prefix = "    ";
+        let expected= format!("{}|RRR|\n\
+            {}|RRR|\n\
+            {}|RRR|\n\
+        |GGG|YYY|BBB|WWW|\n\
+        |GGG|YYY|BBB|WWW|\n\
+        |YYY|BBB|WWW|GGG|\n\
+        {}|OOO|\n\
+        {}|OOO|\n\
+        {}|OOO|\n", prefix, prefix, prefix, prefix, prefix, prefix);
+
+        println!("expected {}", expected);
+        assert_eq!(format!("{}", scrambled_cube), expected);
+    }
+
+    #[test]
+    fn move_ri_rotate_right_multiple_times() {
+        let mut cube = Cube::new([BLUE, RED, YELLOW, ORANGE, WHITE, GREEN]);
+        for _ in 0..6 {
+            cube.move_r();
+            cube.rotate_right();
+        }
+        println!("scrambled!");
+        cube.print();
+        let prefix = "    ";
+        let expected= format!("{}|RWW|\n\
+            {}|BGG|\n\
+            {}|OYY|\n\
+        |WOG|WRR|GWB|OOB|\n\
+        |WOY|GYB|YRR|GWB|\n\
+        |GRY|BOY|OOB|YRR|\n\
+        {}|OGG|\n\
+        {}|BBY|\n\
+        {}|WWR|\n", prefix, prefix, prefix, prefix, prefix, prefix);
+
+        println!("expected {}", expected);
+        assert_eq!(format!("{}", cube), expected);
+    }
+
+    #[test]
+    fn mix_and_fix() {
+        let cube = Cube::new([RED, YELLOW, WHITE, GREEN, BLUE, ORANGE]);
+        let mut mixed_cube = cube.clone();
+        let count = 15;
+
+        for _ in 0..count {
+            mixed_cube.orientation_rotate_left();
+            mixed_cube.move_r();
+        }
+        for _ in 0..count {
+            mixed_cube.move_ri();
+            mixed_cube.orientation_rotate_right();
+        }
+        assert_eq!(format!("{}", cube), format!("{}", mixed_cube));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,24 @@
 mod cube;
 
 fn main() {
-    let mut cube = cube::Cube::default();
+    let cube = cube::Cube::default();
     cube.print();
     println!("Hello, world!");
 
-    cube.orientation_rotate_right();
-    cube.print();
+    let mut mixed_cube = cube.clone();
+    let mut i = 0;
+    loop {
+        println!("------ step {} move --------", i);
+        mixed_cube.move_r();
+        mixed_cube.print();
+        println!("------ step {} rotate --------", i);
+        mixed_cube.rotate_right();
+        mixed_cube.print();
+        i = i + 1;
 
-    cube.orientation_rotate_up();
-    cube.print();
+        if mixed_cube == cube {
+            println!("solved: {}!", i);
+            break;
+        }
+    }
 }
